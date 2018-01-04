66a1113b7ad9252b30526966b9f0ee31a925872e
DimitarToshevski <dimitar_toshevski@abv.bg>
2018-01-04 22:09:42 +0000

    added slide effect to dropdown, fonts colored and sized, all hr's in main body positioned absolute, added a-hrefs to header image and h1 in section offers and about us

--- scripts/homepage_dropdown.js
+++ scripts/homepage_dropdown.js
@@ -2,15 +2,15 @@ function accordeon() {
     setTimeout(function () {
         $('.header_offer_1').find('h4').click(function () {
             if ($(event.target).hasClass('shownContinent')) {
-                $('.shown').hide();
+                $('.shown').slideUp();
                 $('.shown').removeClass('shown');
                 $(event.target).removeClass('shownContinent');
             }
             else {
                 $('.shownContinent').removeClass('shownContinent');
                 $(event.target).addClass('shownContinent');
-                $('.shown').removeClass('shown').hide();
-                $(event.target).parent().next('.body_offer_1').find('p').addClass('shown').show();
+                $('.shown').removeClass('shown').slideUp();
+                $(event.target).parent().next('.body_offer_1').find('p').addClass('shown').slideDown();
 
             }
         })
