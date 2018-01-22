afb86c7ca062b83494a1c4a5f5485aa261020aed
DimitarToshevski <dimitar_toshevski@abv.bg>
2018-01-22 22:08:18 +0000

    added route to video page, updated videos.json

--- scripts/router.js
+++ scripts/router.js
@@ -66,6 +66,29 @@ $(() => {
             })
         });
 
+        this.get('#/videos', function () {
+            $.get('../database/videos.json').then((videos) => {
+                this.video = videos;
+            });
+            this.loadPartials({
+                header_wrapper: './templates/common/header/header_wrapper.hbs',
+                header_logo: './templates/common/header/header_logo.hbs',
+                header_menu: './templates/common/header/header_menu.hbs',
+                main: './templates/video_page/videopage_main_wrapper.hbs',
+                footer_wrapper: './templates/common/footer/footer_wrapper.hbs',
+                footer_section_offers: './templates/common/footer/footer_section_offers.hbs',
+                footer_section_partners: './templates/common/footer/footer_section_partners.hbs',
+                footer_section_follow: './templates/common/footer/footer_section_follow.hbs',
+                contact_us_button: './templates/common/contact_us_button.hbs'
+            }).then(function () {
+                this.partial('./templates/common/page.hbs');
+            }).then(function () {
+                sticky.stickFooter();
+                sticky.stickHeader();
+                scrollTop();
+            })
+        });
+
     });//for deployment we must put router.run('#/')
     router.run();
 });
