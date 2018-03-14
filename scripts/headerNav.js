09760b1fa4190f8a6b7ee48517554dda5bbcc6cf
xavier <war_master@bitex.bg>
2018-03-14 20:34:39 +0000

    header nav button functionality to show/hide menu on click

--- scripts/headerNav.js
+++ scripts/headerNav.js
@@ -1,10 +1,13 @@
 function adaptNav() {
-    $('#btn_menu').on('mouseenter',() => {
-        $('.header_nav').css('display','block');
-        $('#btn_menu').css({"width": "175px", "height": "225px"});
+    let shown = false;
+    $('#btn_menu').on('click', () => {
+        if (!shown) {
+            $('.header_nav').css('display', 'block');
+            shown = true;
+        } else {
+            $('.header_nav').css('display', 'none');
+            shown = false;
+        }
     });
 
-    $('#btn_menu').on('mouseleave',() => {
-        $('.header_nav').css('display','none');
-    })
 }
