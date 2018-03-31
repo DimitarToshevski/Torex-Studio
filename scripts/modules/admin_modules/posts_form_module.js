acf3f1f935950298b71bb4e81013e22aa701d537
xavier <war_master@bitex.bg>
2018-03-31 14:24:34 +0100

    added show/hide functionality and buttons on video and post admin forms, added toastr msg for success + disabling/enabling on submit button

--- scripts/modules/admin_modules/posts_form_module.js
+++ scripts/modules/admin_modules/posts_form_module.js
@@ -2,28 +2,41 @@ import {requestData} from "../requester";
 
 let attachPostsFormEvents = () => {
     setTimeout(() => {
-        $('#submit_post').on('submit', (e) => {
-            e.preventDefault();
-            let title = $('#post_title').val();
-            let subtitle = $('#post_subtitle').val();
-            let postImg = $('#post_img').val();
-            let postVideo = $('#post_video').val();
-            let postBody = $('#post_body').val();
-            let postBody2 = $('#post_body2').val();
-            let reqBody = JSON.stringify({
-                "id": "18",
-                title,
-                subtitle,
-                "img_src": postImg,
-                "video_url": postVideo,
-                "body": postBody,
-                "body2": postBody2
+        $('#show_post_form').click(() => {
+
+            $('#submit_post').css('display', 'flex'); //showing form
+
+            $('#close_post_form').click(() => { //attaching event listener for close button on form action = hide form
+                $('#submit_post').css('display', 'none')
             });
-            requestData('appdata', 'posts', '', 'POST', reqBody).then((data) => {
-                console.log(data);
-            });
-            return false;
+
+            $('#submit_post').on('submit', (e) => {
+                e.preventDefault();
+                $('.submitData').attr('disabled', 'true'); //disabling button so there are no multiple requests
+                let title = $('#post_title').val();
+                let subtitle = $('#post_subtitle').val();
+                let postImg = $('#post_img').val();
+                let postVideo = $('#post_video').val();
+                let postBody = $('#post_body').val();
+                let postBody2 = $('#post_body2').val();
+                let reqBody = JSON.stringify({
+                    "id": "18",
+                    title,
+                    subtitle,
+                    "img_src": postImg,
+                    "video_url": postVideo,
+                    "body": postBody,
+                    "body2": postBody2
+                });
+                requestData('appdata', 'posts', '', 'POST', reqBody).then((post) => {
+                    toastr.success(`Успешно качен пост: ${post.title} <br> ${post.subtitle}`);
+                    setTimeout(() => {
+                        $('.submitData').removeAttr('disabled'); //enabling submit button
+                    }, 2000)
+                });
+                return false;
+            })
         })
-    }, 3000)
+    }, 100);
 };
 export { attachPostsFormEvents }
--- scripts/modules/admin_modules/videos_form_module.js
+++ scripts/modules/admin_modules/videos_form_module.js
@@ -3,25 +3,35 @@ import { requestData } from "../requester";
 let regexVideoUrl = /\/\/www.youtube.com\/embed\/\w*/g;
 let attachVideosFormEvents = (ctx) => {
     setTimeout(() => {
-        $('#submit_video').on('submit', (e) => {
-            e.preventDefault();
-            let video_url = $('#video_url').val().match(regexVideoUrl)[0];
-            let video_title = $('#video_title').val();
-            let img_url = $('#video_img').val();
-            let type = $('#video_type option:selected').val();
-            let reqBody = JSON.stringify({
-                video_url,
-                video_title,
-                img_url,
-                type
+        $('#show_video_form').click(() => {
+            $('#submit_video').css('display', 'flex'); //showing form
+
+            $('#close_video_form').click(() => { //attaching event listener for close button on form action = hide form
+                $('#submit_video').css('display', 'none')
             });
-            requestData('appdata', 'videos', '', 'POST', reqBody).then((videos) => {
-                toastr.success(`Успешно качен клип: ${videos.video_title}`);
-                ctx.render('./templates/video_page/single_video_partial.hbs')
-                    .then(ctx.replace('#gallery'));
-            });
-            return false;
+
+            $('#submit_video').on('submit', (e) => { //attaching event listener to upload button
+                e.preventDefault();
+                $('.submitData').attr('disabled', 'true'); //disabling button so there are no multiple requests
+                let video_url = $('#video_url').val().match(regexVideoUrl)[0];
+                let video_title = $('#video_title').val();
+                let img_url = $('#video_img').val();
+                let type = $('#video_type option:selected').val();
+                let reqBody = JSON.stringify({
+                    video_url,
+                    video_title,
+                    img_url,
+                    type
+                });
+                requestData('appdata', 'videos', '', 'POST', reqBody).then((videos) => {
+                    toastr.success(`Успешно качен клип: ${videos.video_title}`);
+                    setTimeout(() => {
+                        $('.submitData').removeAttr('disabled'); //enabling submit button
+                    }, 2000)
+                });
+                return false;
+            })
         })
-    }, 3000)
+    }, 100)
 };
 export { attachVideosFormEvents }
