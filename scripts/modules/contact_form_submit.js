dab2bca3f3b95a68e149f49dcc46c626e867ad1f
xavier <war_master@bitex.bg>
2018-04-12 22:50:13 +0100

    updated contact form submit module

--- scripts/modules/contact_form_submit.js
+++ scripts/modules/contact_form_submit.js
@@ -1,7 +1,6 @@
 let contactFormSubmit = () => {
     setTimeout(() => {
         $('#send_message').on('click', (e) => {
-            alert('asdsa');
             let sender = $('#name').val();
             let email = $('#email').val();
             let message = $('#message').val();
