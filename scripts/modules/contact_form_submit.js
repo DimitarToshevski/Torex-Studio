3732ab81beefbee79eabafa76be0bfadb27b60df
xavier <war_master@bitex.bg>
2018-04-13 23:27:11 +0100

    added some validation on the inputs of contact form

--- scripts/modules/contact_form_submit.js
+++ scripts/modules/contact_form_submit.js
@@ -11,6 +11,31 @@ let contactFormSubmit = () => {
             let newDate = new Date();
             let date = `${newDate.getDate()} ${months[newDate.getMonth()]} ${newDate.getFullYear()}`;
             let exactTime = `${newDate.getHours()} ${newDate.getMinutes()} ${newDate.getSeconds()}`;
+            //VALIDATION
+            if(senderName.match(/(\s\s+)+/g) || senderName === '' || senderName === ' ') {
+                toastr.error('Моля въведи валидно име.');
+                setTimeout(() => {
+                    $('#send_message').removeAttr('disabled'); //enabling submit button
+                }, 1000);
+                return;
+            }
+
+            if(contactEmail.match(/(\s\s+)+/g) || contactEmail === '' || contactEmail === ' ' || !contactEmail.match(/[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]+/g)) {
+                toastr.error('Моля въведи валиден имейл адрес.');
+                setTimeout(() => {
+                    $('#send_message').removeAttr('disabled'); //enabling submit button
+                }, 1000);
+                return;
+            }
+
+            if(message.match(/(\s\s+)+/g) || message === '' || message === ' ') {
+                toastr.error('Моля въведи валидно съобщение.');
+                setTimeout(() => {
+                    $('#send_message').removeAttr('disabled'); //enabling submit button
+                }, 1000);
+                return;
+            }
+
             let reqBody = JSON.stringify({
                 senderName,
                 contactEmail,
@@ -19,7 +44,7 @@ let contactFormSubmit = () => {
                 exactTime
             });
             requestData('appdata', 'messages', '', 'POST', reqBody).then((message) => {
-                $('#message_form').find('input[type=text], textarea').val('');
+                $('#message_form').find('input[type=text], input[type=email], textarea').val('');
                 toastr.success(`Благодарим Ви за съобщението, ${message.senderName}!`);
                 setTimeout(() => {
                     $('#send_message').removeAttr('disabled'); //enabling submit button
