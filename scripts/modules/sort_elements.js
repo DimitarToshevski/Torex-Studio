eca512923febe315c836dbdbbc90c3bde10d8c64
xavier <war_master@bitex.bg>
2018-04-27 19:00:42 +0100

    updated messages in header nav position, updated photo heading not to be required in admin input forms, added regex for date picking in sort module

--- scripts/modules/sort_elements.js
+++ scripts/modules/sort_elements.js
@@ -1,7 +1,9 @@
-const dayRegex = /(\d|\d\d)\s/g;
+const dateRegex = /(.*)\s(.*)\s(.*)/g;
 
 let sortElements = (elements) => {
     elements = elements.sort((a, b) => {
+        let dateA = dateRegex.exec(a.date);
+        let dateB = dateRegex.exec(b.date);
         if(a.date.localeCompare( b.date) === 0) {
             return (b.exact_time.localeCompare( a.exact_time))
         } else {
